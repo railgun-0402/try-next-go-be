5c971a8b0288e9851ba0e39175e4c353d2f5cd83
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-04-30 23:16:28 +0900

    不要コード削除

--- main.go
+++ main.go
@@ -11,12 +11,11 @@ func main() {
 	r := gin.Default()
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "test is passed!",
+			"message": "EKS Deploy Is Success!!!",
 		})
 	})
 
 	log.Println("server start at port 8080")
 	r.Run(":8080")
-	// log.Fatal(http.ListenAndServe(":8080", r))
 }
 
